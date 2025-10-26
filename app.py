ef33084b840449b526a4db5e01b5eff93b8caf1a
Kapil6996 <kapillkumar69@gmail.com>
2025-10-26 23:28:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,14 @@ if uploaded_file is not None:
         0: {"name": "Cardboard", "recyclable": True, "tip": "Rinse and sort plastics by type before recycling."},
         1: {"name": "Glass", "recyclable": True, "tip": "Keep paper clean and dry. Avoid mixing with food waste."},
         2: {"name": "Metal", "recyclable": True, "tip": "Crush cans to save space and remove any labels if possible."},
-        3: {"name": "Paper", "recyclable": True, "tip": "Rinse glass containers and separate by color if required."},
+        3: {"name": "Paper", "recyclable": True, "tip": "Rinse glass containers and separate by color if required.","tips": "1.Newspaper/Magazine Pages:	Woven Baskets or Coasters:	Roll the paper tightly into long, thin tubes or strips, then weave them together, securing them with glue, to create durable baskets, placemats, or coasters.
+
+"2.Junk Mail/Scraps:	Shredded Packing Material:	Run unwanted paper (excluding glossy or photo paper) through a shredder to create simple, effective cushioning for shipping parcels."
+
+"3. Magazine Pages:	Decoupage:	Cut out colourful images or text and glue them onto furniture, picture frames, or boxes for a unique, patterned finish."
+
+"4. Paper Bags:	DIY Book Covers:	Cut large paper bags open and use them to cover school books or notebooks, protecting them from damage"},
+
         4: {"name": "Plastic", "recyclable": True, "tip": "Check the recycling number. #1 and #2 are most recyclable.","tips": "1. Plastic Bottles: Hanging Planters / Herb Garden: Cut large soda bottles horizontally or vertically. Puncture drainage holes, decorate the outside, and hang them by string or wire to create a space-saving wall garden.    "
            "2.    Bottle Bottoms: Flower Decorations:	Cut off the rigid, patterned bottoms of large soda bottles, paint them bright colours, and string them together to create garlands, wind chimes, or faux flowers.    "
            "3.    Lotion/Shampoo Bottles:	Charging Phone Holder:	Cut a slot in the back of an empty, clean bottle (near the neck) to hang over an electrical adapter while the phone rests in the body of the bottle during charging.    "
